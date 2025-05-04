ccd2b8835cb2302b5dbbb8ccc0a36d2b8334517b
Sunilbasnet-max <sunilbasnet4893@gmail.com>
2025-05-04 22:46:46 +0545

    qucik change

--- src/helpers/formatter.js
+++ src/helpers/formatter.js
@@ -4,6 +4,7 @@
             id:data.id,
             name:data.name,
             email:data.email,
+            number:data.number,
             profileImageUrl:data.profileImageUrl,
             roles:data.roles,
             createAt:data.createdAt
