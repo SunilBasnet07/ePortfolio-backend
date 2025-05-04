a348066d933910fc44470be8d2939829350aa4b6
Sunilbasnet-max <sunilbasnet4893@gmail.com>
2025-05-04 22:40:42 +0545

    quick change

--- src/helpers/formatter.js
+++ src/helpers/formatter.js
@@ -4,7 +4,6 @@
             id:data.id,
             name:data.name,
             email:data.email,
-            phone:data.phone,
             profileImageUrl:data.profileImageUrl,
             roles:data.roles,
             createAt:data.createdAt
